df1e7dd042b7c997e9e1d91a8eca646725bf5b65
Floyd <41008398+StrawberryMaster@users.noreply.github.com>
2021-07-20 18:23:31 -0300

    Fixed typo
    All questions should work now

--- questions.js
+++ questions.js
@@ -7,13 +7,13 @@ questions =  [
     {"id": 5, "question": "The wiki leader/server owner should be able to make decisions by themself without a consensus.", "effects": {"strict": 1}},
     {"id": 6, "question": "Complaining and/or ranting about tropical cyclones or weather is childish behavior and should be punished.", "effects": {"right": -1}},
     {"id": 7, "question": "The category difference between a 155 mph and 160 mph hurricane is important.", "effects": {"right": 1}},
-    {"id": 7, "question": "Users should always focus their attention on the more threatening storm to track, regardless of what basin it is in.", "effects": {"right": -1}},
-    {"id": 8, "question": "StrawberryMaster was one of the most powerful users ahead of June 19.", "effects": {"right": -1}},
-    {"id": 9, "question": "The HHW administration was corrupt prior to the Electiongate scandal, and deserved to be ousted.", "effects": {"right": -1}},
-    {"id": 10, "question": "Users should be given indefinite bans when it is not clear if they will improve.", "effects": {"strict": 1}},
-    {"id": 11, "question": "NSFW discussion should not be punished with mutes right away.", "effects": {"strict": -1}},
-    {"id": 12, "question": "Reaction GIFs should be allowed in #casual-chat.", "effects": {"strict": -1}},
-    {"id": 13, "question": "HHW chat was more fun several years ago than it is today.", "effects": {"right": 1}},
-    {"id": 14, "question": "The decision to overturn the punishments in the GM incident was the right decision.", "effects": {"strict": -1}},
-    {"id": 15, "question": "Random foreign language words should not be allowed in #casual-chat.", "effects": {"strict": 1}}
+    {"id": 8, "question": "Users should always focus their attention on the more threatening storm to track, regardless of what basin it is in.", "effects": {"right": -1}},
+    {"id": 9, "question": "StrawberryMaster was one of the most powerful users ahead of June 19.", "effects": {"right": -1}},
+    {"id": 10, "question": "The HHW administration was corrupt prior to the Electiongate scandal, and deserved to be ousted.", "effects": {"right": -1}},
+    {"id": 11, "question": "Users should be given indefinite bans when it is not clear if they will improve.", "effects": {"strict": 1}},
+    {"id": 12, "question": "NSFW discussion should not be punished with mutes right away.", "effects": {"strict": -1}},
+    {"id": 13, "question": "Reaction GIFs should be allowed in #casual-chat.", "effects": {"strict": -1}},
+    {"id": 14, "question": "HHW chat was more fun several years ago than it is today.", "effects": {"right": 1}},
+    {"id": 15, "question": "The decision to overturn the punishments in the GM incident was the right decision.", "effects": {"strict": -1}},
+    {"id": 16, "question": "Random foreign language words should not be allowed in #casual-chat.", "effects": {"strict": 1}}
 ];
