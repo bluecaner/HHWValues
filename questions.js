68f47676e3da8cd5bd6a44038ffdb537788ddf3e
Floyd <41008398+StrawberryMaster@users.noreply.github.com>
2021-07-21 11:07:50 -0300

    Cleanup

--- questions.js
+++ questions.js
@@ -1,28 +1,158 @@
-questions =  [
-    {"id": 0, "question": "HHW needs Storm Mode to prevent jokes and arguments during a threatening tropical cyclone.", "effects": {"strict": 1}},
-    {"id": 1, "question": "HHW should strongly encourage, or even require, users to support \"basin neutrality\".", "effects": {"right": -1}},
-    {"id": 2, "question": "Staff members that do not moderate the chat frequently enough should be demoted.", "effects": {"right": -1}},
-    {"id": 3, "question": "COPPA should not be followed.", "effects": {"right": -1}},
-    {"id": 4, "question": "Measures like staff reductions and other role changes are benefitial to the community.", "effects": {"right": -1"}},
-    {"id": 5, "question": "The wiki leader/server owner should be able to make decisions by themself without a consensus.", "effects": {"strict": 1}},
-    {"id": 6, "question": "Complaining and/or ranting about tropical cyclones or weather is childish behavior and should be punished.", "effects": {"right": -1}},
-    {"id": 7, "question": "The category difference between a 155 mph and 160 mph hurricane is important.", "effects": {"right": 1}},
-    {"id": 8, "question": "Users should always focus their attention on the more threatening storm to track, regardless of what basin it is in.", "effects": {"right": -1}},
-    {"id": 9, "question": "HHW needs to move to Hypoverse or become independent from Fandom.", "effects": {"right": -1}},
-    {"id": 10, "question": "I support a 1-1-1-1-1 administration team.", "effects": {"right": -1}},
-    {"id": 11, "question": "Users should be given indefinite bans when it is not clear if they will improve.", "effects": {"strict": 1}},
-    {"id": 12, "question": "NSFW discussion should not be punished with mutes right away.", "effects": {"strict": -1}},
-    {"id": 13, "question": "Memes and GIFs should be allowed in #casual-chat.", "effects": {"strict": -1}},
-    {"id": 14, "question": "HHW chat was more fun several years ago than it is today.", "effects": {"right": 1}},
-    {"id": 15, "question": "The fanfic era was good and should be revived.", "effects": {"right": -1}},
-    {"id": 16, "question": "Random foreign language words should not be allowed in #casual-chat.", "effects": {"strict": 1}},
-    {"id": 17, "question": "Former moderators who broke Discord's Terms of Service should be allowed a chance of being promoted to staff later on.", "effects": {"strict": -1}},
-    {"id": 18, "question": "The wiki is more important than the server.", "effects": {"right": -0.5}},
-    {"id": 19, "question": "Measures to control disruptive behavior are usually bad.", "effects": {"strict": -0.5}},
-    {"id": 20, "question": "People I dislike should be banned, even if they haven't violated any rules.", "effects": {"right": -0.5}},
-    {"id": 21, "question": "Outsiders who are not active on the wiki should not have any influence on major wiki decisions.", "effects": {"right": -1}},
-    {"id": 22, "question": "All staff should participate in yearly or bi-yearly elections.", "effects": {"right": -1}},    
-    {"id": 23, "question": "Administration members should be able to jump positions in an unorthodox way.", "effects": {"right": -1}},
-    {"id": 24, "question": "Trials are good and should be used more often.", "effects": {"right": -0.5}},
-    {"id": 25, "question": "Hurricane Idol rounds should be decided by HHW users who are eligible to vote, and not the judges.", "effects": {"right": -1}}
-];
+questions = [{
+    "id": 0,
+    "question": "HHW needs Storm Mode to prevent jokes and arguments during a threatening tropical cyclone.",
+    "effects": {
+        "strict": 1
+    }
+}, {
+    "id": 1,
+    "question": "HHW should strongly encourage, or even require, users to support \"basin neutrality\".",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 2,
+    "question": "Staff members that do not moderate the chat frequently enough should be demoted.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 3,
+    "question": "COPPA should not be followed.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 4,
+    "question": "Measures like staff reductions and other role changes are benefitial to the community.",
+    "effects": {
+        "right": -1,
+        "strict": 0.5
+    }
+}, {
+    "id": 5,
+    "question": "The wiki leader/server owner should be able to make decisions by themself without a consensus.",
+    "effects": {
+        "strict": 1
+    }
+}, {
+    "id": 6,
+    "question": "Complaining and/or ranting about tropical cyclones or weather is childish behavior and should be punished.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 7,
+    "question": "The category difference between a 155 mph and 160 mph hurricane is important.",
+    "effects": {
+        "right": 1
+    }
+}, {
+    "id": 8,
+    "question": "Users should always focus their attention on the more threatening storm to track, regardless of what basin it is in.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 9,
+    "question": "HHW needs to move to Hypoverse or become independent from Fandom.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 10,
+    "question": "I support a 1-1-1-1-1 administration team.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 11,
+    "question": "Users should be given indefinite bans when it is not clear if they will improve.",
+    "effects": {
+        "strict": 1
+    }
+}, {
+    "id": 12,
+    "question": "NSFW discussion should not be punished with mutes right away.",
+    "effects": {
+        "strict": -1
+    }
+}, {
+    "id": 13,
+    "question": "Memes and GIFs should be allowed in #casual-chat.",
+    "effects": {
+        "strict": -1
+    }
+}, {
+    "id": 14,
+    "question": "HHW chat was more fun several years ago than it is today.",
+    "effects": {
+        "right": 1
+    }
+}, {
+    "id": 15,
+    "question": "The fanfic era was good and should be revived.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 16,
+    "question": "Random foreign language words should not be allowed in #casual-chat.",
+    "effects": {
+        "strict": 1
+    }
+}, {
+    "id": 17,
+    "question": "Former moderators who broke Discord's Terms of Service should be allowed a chance of being promoted to staff later on.",
+    "effects": {
+        "strict": -1
+    }
+}, {
+    "id": 18,
+    "question": "The wiki is more important than the server.",
+    "effects": {
+        "right": -0.5
+    }
+}, {
+    "id": 19,
+    "question": "Measures to control disruptive behavior are usually bad.",
+    "effects": {
+        "strict": -0.5
+    }
+}, {
+    "id": 20,
+    "question": "People I dislike should be banned, even if they haven't violated any rules.",
+    "effects": {
+        "right": -0.5
+    }
+}, {
+    "id": 21,
+    "question": "Outsiders who are not active on the wiki should not have any influence on major wiki decisions.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 22,
+    "question": "All staff should participate in yearly or bi-yearly elections.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 23,
+    "question": "Administration members should be able to jump positions in an unorthodox way.",
+    "effects": {
+        "right": -1
+    }
+}, {
+    "id": 24,
+    "question": "Trials are good and should be used more often.",
+    "effects": {
+        "right": -0.5
+    }
+}, {
+    "id": 25,
+    "question": "Hurricane Idol rounds should be decided by HHW users who are eligible to vote, and not the judges.",
+    "effects": {
+        "right": -1
+    }
+}];
